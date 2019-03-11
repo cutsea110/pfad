0200df80efda51832c5d8faa04eff55790f6d899
cutsea110 <cutsea110@gmail.com>
2019-03-11 09:37:20 +0900

    simplified hylo (deforest ver.)

--- Nexus.hs
+++ Nexus.hs
@@ -2,17 +2,24 @@ module Nexus where
 
 data Tree a = Leaf a | Node [Tree a] deriving (Show, Eq)
 
+{--
 foldt (f, g) = u
   where
     u (Leaf x) = f x
     u (Node xs) = g (fmap u xs)
-
+--}
+foldt f g (Leaf x) = f x
+foldt f g (Node ts) = g (fmap (foldt f g) ts)
+{--
 unfoldt psi = v
   where
     v x = case psi x of
       Left x -> Leaf x
       Right xs -> Node (fmap v xs)
-
+--}
+unfoldt p v h x = if p x then Leaf (v x)
+                  else Node (fmap (unfoldt p v h) (h x))
+{--
 -- hylot f g = foldt f . unfoldt g
 
 -- deforestation!
@@ -21,3 +28,9 @@ hylot f g = h
     h x = case g x of
             Left x -> f (Left x)
             Right xs -> f (Right (fmap h xs))
+--}
+
+-- hylot f g p v h = foldt f g . unfoldt p v h
+-- hylot f g p h x = if p x then f x else g (fmap (hylot f g p h) (h x))
+hylot f g p h x | p x       = f x
+                | otherwise = g (fmap (hylot f g p h) (h x))
